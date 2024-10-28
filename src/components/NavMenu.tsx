d26bb8ccd71ab9406ed9f83716af1630f8c46e93
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-28 09:55:18 +0100

    added navbar buttons

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {
+    ArrowLeftEndOnRectangleIcon,
+    CalendarIcon,
+    UserGroupIcon,
+    UserIcon,
+} from '@heroicons/react/24/solid'
 
 interface MenuProps {
     isOpen: boolean
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+interface Buttonprops {
+    icon: JSX.Element
+    title: string
+    href: string
+}
+
+const MenuButton = ({ icon, title, href }: Buttonprops): JSX.Element => {
+    return (
+        <button
+            type="button"
+            className="flex p-2 border-b-2 items-center w-full text-base text-gray-900 transition duration-75  hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+            onClick={() => location.replace(href)}
+        >
+            {icon}
+            <p className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
+                {title}
+            </p>
+        </button>
+    )
 }
 
 const NavMenu = ({ isOpen, setIsOpen }: MenuProps): JSX.Element => {
@@ -21,10 +48,29 @@ const NavMenu = ({ isOpen, setIsOpen }: MenuProps): JSX.Element => {
                     (isOpen ? ' translate-x-0 ' : ' translate-x-full ')
                 }
             >
-                <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
-                    <header className="p-4 font-bold text-lg">Header</header>
-                    test
-                </article>
+                <h1 className="p-4 text-xl border-b-2">Menu</h1>
+                <MenuButton
+                    icon={<CalendarIcon className="size-6  text-red-700" />}
+                    title={'Bookings'}
+                    href={'/'}
+                />
+                <MenuButton
+                    icon={<UserGroupIcon className="size-6 text-red-700" />}
+                    title={'Groups'}
+                    href={'/groups'}
+                />
+                <MenuButton
+                    icon={<UserIcon className="size-6 text-red-700" />}
+                    title={'Edit profile'}
+                    href={'/user'}
+                />
+                <MenuButton
+                    icon={
+                        <ArrowLeftEndOnRectangleIcon className="size-6 text-red-700" />
+                    }
+                    title={'Logout'}
+                    href={'/logout'}
+                />
             </section>
             <section
                 className=" w-screen h-full cursor-pointer "
