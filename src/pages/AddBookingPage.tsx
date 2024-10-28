24e83ac56bf2246313a2ab04f83230a84605ea31
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-28 13:16:42 +0100

    added first draft of add-booking

--- src/pages/AddBookingPage.tsx
+++ src/pages/AddBookingPage.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React from 'react'
 
 const AddNewBookingPage: React.FC = () => {
-	return (
-		<div>
-			<h1>Welcome to AddNewBookingPage</h1>
-		</div>
-	);
-};
+    const handleSubmit = (e: React.FormEvent): void => {
+        e.preventDefault()
+        const { title, dateFrom, dateTo, participants } =
+            e.target as HTMLFormElement
+    }
 
-export default AddNewBookingPage;
+    return (
+        <form className="p-4 flex flex-col gap-3" onSubmit={handleSubmit}>
+            <label className="">
+                <p className="pl-2">Booking title</p>
+                <input required className="input-contained" name="title" />
+            </label>
+
+            <div className="flex gap-3">
+                <label className="">
+                    <p className="pl-2">Date from</p>
+                    <input
+                        required
+                        className="input-contained"
+                        name="dateFrom"
+                    />
+                </label>
+                <label className="">
+                    <p className="pl-2">Date to</p>
+                    <input required className="input-contained" name="dateTo" />
+                </label>{' '}
+            </div>
+
+            <label className="p-2">
+                Participants
+                <input
+                    required
+                    className="input-contained"
+                    name="participants"
+                />
+            </label>
+            <button className="btn-primary ml-2" type="submit">
+                Save
+            </button>
+        </form>
+    )
+}
+
+export default AddNewBookingPage
