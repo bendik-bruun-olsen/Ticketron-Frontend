73e840d022670f9dd66fe0bd95c5aace070ff028
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-28 13:31:34 +0100

    fix/21-move-bookings-to-home

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,51 @@
-// pages/HomePage.tsx
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { BookingCard } from '../components/BookingCard'
+import BookingsList from '../components/BookingsList'
 
-const Homepage: React.FC = () => {
+const BookingDummy = {
+    imageUrl: 'https://placehold.co/173x173',
+    title: 'Trip to Paris',
+    fromDate: 'June 10, 2023',
+    toDate: 'June 12, 2023',
+    participants: 1,
+    id: '1',
+}
+
+const bookings = [
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+]
+
+const HomePage: React.FC = () => {
     return (
-        <div>
-            <h2>Welcome to the Home Page</h2>
-            <p>You are successfully logged in!</p>
+        <div className="p-4 flex flex-col gap-10">
+            <h1 className="text-2xl font-bold">Welcome back, User!</h1>
+            <BookingsList bookings={bookings} />
         </div>
     )
 }
 
-export default Homepage
+export default HomePage
