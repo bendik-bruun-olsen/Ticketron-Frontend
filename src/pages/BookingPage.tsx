35ed4a1642f22dd506d44575dfcb2c33b426d6d4
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-28 10:27:19 +0100

    delete output css from repo and fix children

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { BookingCard } from '../components/BookingCard'
+
+const BookingDummy = {
+    imageUrl: 'https://placehold.co/173x173',
+    title: '',
+    fromDate: 'June 10, 2023',
+    toDate: 'June 12, 2023',
+    participants: 1,
+    id: '1',
+}
 
 const BookingPage: React.FC = () => {
-	return (
-		<div>
-			<Navbar />
-			<h2>Welcome to the booking page</h2>
-		</div>
-	);
-};
+    return (
+        <div>
+            <h2>Welcome to the booking page</h2>
+            <BookingCard booking={BookingDummy} />
+        </div>
+    )
+}
 
-export default BookingPage;
+export default BookingPage
