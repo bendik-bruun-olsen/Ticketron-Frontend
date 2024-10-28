1d5b82162bd738f28763d2f79b11b1463a3bc88b
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-28 09:56:23 +0100

    pulled menubutton into new file

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -5,31 +5,11 @@ import {
     UserGroupIcon,
     UserIcon,
 } from '@heroicons/react/24/solid'
+import MenuButton from './NavButton'
 
 interface MenuProps {
     isOpen: boolean
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-interface Buttonprops {
-    icon: JSX.Element
-    title: string
-    href: string
-}
-
-const MenuButton = ({ icon, title, href }: Buttonprops): JSX.Element => {
-    return (
-        <button
-            type="button"
-            className="flex p-2 border-b-2 items-center w-full text-base text-gray-900 transition duration-75  hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            onClick={() => location.replace(href)}
-        >
-            {icon}
-            <p className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
-                {title}
-            </p>
-        </button>
-    )
 }
 
 const NavMenu = ({ isOpen, setIsOpen }: MenuProps): JSX.Element => {
