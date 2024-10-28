2e205dedbc7f278abe48fbae0016fa76482d7ee6
Bendik Bruun-Olsen <bendik.bruun-olsen@bouvet.no>
2024-10-28 14:55:54 +0100

    init commit

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,15 +1,48 @@
-/// <reference types="vite/client" />
+import { LogLevel } from '@azure/msal-browser'
+
+const isDevEnv = process.env.NODE_ENV === 'development'
+
 export const msalConfig = {
-  auth: {
-    clientId: import.meta.env.VITE_AZURE_AD_CLIENT_ID,
-    authority: `https://login.microsoftonline.com/${
-      import.meta.env.VITE_AZURE_AD_TENANT_ID
-    }`,
-    redirectUri: import.meta.env.VITE_AZURE_AD_REDIRECT_URI,
-  },
-};
+    auth: {
+        clientId: '4aa092b2-22e7-4cb7-b379-da3a640f27e0',
+        authority: 'https://login.microsoftonline.com/organizations',
+        redirectUri: isDevEnv
+            ? 'http://localhost:5173/auth/callback'
+            : 'https://white-cliff-06c7dbb03.5.azurestaticapps.net/3/auth/callback',
+    },
+    system: isDevEnv
+        ? {
+              loggerOptions: {
+                  loggerCallback: (
+                      level: LogLevel,
+                      message: string,
+                      containsPii: boolean
+                  ) => {
+                      if (containsPii) {
+                          return
+                      }
+                      switch (level) {
+                          case LogLevel.Error:
+                              console.error(message)
+                              return
+                          case LogLevel.Info:
+                              console.info(message)
+                              return
+                          case LogLevel.Verbose:
+                              console.debug(message)
+                              return
+                          case LogLevel.Warning:
+                              console.warn(message)
+                              return
+                          default:
+                              return
+                      }
+                  },
+              },
+          }
+        : {},
+}
 
 export const loginRequest = {
-  scopes: ["user.read"],
-  prompt: "select_account",
-};
+    scopes: ['api://75d36570-3953-44a8-a8d7-ef388c3703fc/User.ReadWrite'],
+}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,130 @@
-// pages/HomePage.tsx
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import { BookingCard } from '../components/BookingCard'
+import BookingsList from '../components/BookingsList'
+import { useMsal } from '@azure/msal-react'
+import { loginRequest } from '../authConfig'
+import Logout from '../components/Logout'
 
-const Homepage: React.FC = () => {
+const BookingDummy = {
+    imageUrl: 'https://placehold.co/173x173',
+    title: 'Trip to Paris',
+    fromDate: 'June 10, 2023',
+    toDate: 'June 12, 2023',
+    participants: 1,
+    id: '1',
+}
+
+const bookings = [
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+]
+
+interface ApiConfig {
+    method: 'get' | 'post' | 'put' | 'delete'
+    url: string
+    headers?: Record<string, string>
+}
+
+const HomePage: React.FC = () => {
+    const [data, setData] = useState({})
+    const { instance, accounts } = useMsal()
+    const isAuthenticated = accounts.length > 0
+    let apiConfig = {} as ApiConfig
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log('Handling form submission')
+
+        const Id = e.currentTarget.search.value
+        if (Id) {
+            apiConfig = {
+                method: 'get',
+                url: `https://ticketron-web-cbbednduhza8hwgt.northeurope-01.azurewebsites.net/api/Booking/${Id}`,
+            }
+
+            console.log('Fetching data in handleSubmit')
+            fetchData(apiConfig)
+        } else {
+            console.log('Error in handleSubmit')
+        }
+    }
+
+    const acquireToken = async () => {
+        const request = { ...loginRequest, account: accounts[0] }
+        const response = await instance.acquireTokenSilent(request)
+
+        return response.accessToken
+    }
+
+    const fetchData = async (apiConfig: ApiConfig) => {
+        const readyToFetch = apiConfig && isAuthenticated
+        if (!readyToFetch) return
+
+        try {
+            const token = await acquireToken()
+            if (token) console.log('Token acquired: ', token)
+
+            console.log('Modifying apiConfig: ', apiConfig)
+
+            const configWithToken = {
+                ...apiConfig,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+
+            console.log('Fetching data with modified config: ', configWithToken)
+
+            const response = await fetch(apiConfig.url, {
+                method: apiConfig.method,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            if (!response.ok) {
+                console.log('No response from API :(')
+                return
+            }
+            setData(response)
+        } catch (error) {
+            console.log('Error fetching data: ', error)
+        }
+    }
+
     return (
-        <div>
-            <h2>Welcome to the Home Page</h2>
-            <p>You are successfully logged in!</p>
+        <div className="p-4 flex flex-col gap-10">
+            <h1 className="text-2xl font-bold">Welcome back, User!</h1>
+            <Logout />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="search" />
+                <button type="submit">Submit</button>
+            </form>
+            <BookingsList bookings={bookings} />
         </div>
     )
 }
 
-export default Homepage
+export default HomePage
