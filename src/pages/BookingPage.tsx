2854040166a16054cdbb086245299d26c03f3083
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-28 10:54:44 +0100

    added bookingcard and bookinglist

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,21 +1,49 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { BookingCard } from '../components/BookingCard'
+import BookingsList from '../components/BookingsList'
 
 const BookingDummy = {
     imageUrl: 'https://placehold.co/173x173',
-    title: '',
+    title: 'Trip to Paris',
     fromDate: 'June 10, 2023',
     toDate: 'June 12, 2023',
     participants: 1,
     id: '1',
 }
 
+const bookings = [
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+    {
+        imageUrl: 'https://placehold.co/173x173',
+        title: 'Trip to Paris',
+        fromDate: 'June 10, 2023',
+        toDate: 'June 12, 2023',
+        participants: 1,
+        id: '1',
+    },
+]
+
 const BookingPage: React.FC = () => {
     return (
-        <div>
-            <h2>Welcome to the booking page</h2>
-            <BookingCard booking={BookingDummy} />
+        <div className="p-4 flex flex-col gap-10">
+            <h1 className="text-2xl font-bold">Welcome back, User!</h1>
+            <BookingsList bookings={bookings} />
         </div>
     )
 }
