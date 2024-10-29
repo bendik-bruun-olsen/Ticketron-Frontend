cb0a2df462aa785328574f4f4ae2a3c7635c6103
Bendik Bruun-Olsen <bendik.bruun-olsen@bouvet.no>
2024-10-29 11:23:36 +0100

    Cleanup in authConfig

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -2,48 +2,41 @@
 
 import { LogLevel } from '@azure/msal-browser'
 
-const isDevEnv = process.env.NODE_ENV === 'development'
-
 export const msalConfig = {
     auth: {
         clientId: import.meta.env.VITE_AZURE_AD_CLIENT_ID,
         authority: import.meta.env.VITE_AZURE_AD_AUTHORITY,
-        // redirectUri: isDevEnv
-        //     ? import.meta.env.VITE_AZURE_AD_DEV_REDIRECT_URI
-        //     : import.meta.env.VITE_AZURE_AD_PROD_REDIRECT_URI,
         redirectUri: import.meta.env.BASE_URL + 'auth/callback',
     },
-    system: isDevEnv
-        ? {
-              loggerOptions: {
-                  loggerCallback: (
-                      level: LogLevel,
-                      message: string,
-                      containsPii: boolean
-                  ) => {
-                      if (containsPii) {
-                          return
-                      }
-                      switch (level) {
-                          case LogLevel.Error:
-                              console.error(message)
-                              return
-                          case LogLevel.Info:
-                              console.info(message)
-                              return
-                          case LogLevel.Verbose:
-                              console.debug(message)
-                              return
-                          case LogLevel.Warning:
-                              console.warn(message)
-                              return
-                          default:
-                              return
-                      }
-                  },
-              },
-          }
-        : {},
+    system: {
+        loggerOptions: {
+            loggerCallback: (
+                level: LogLevel,
+                message: string,
+                containsPii: boolean
+            ) => {
+                if (containsPii) {
+                    return
+                }
+                switch (level) {
+                    case LogLevel.Error:
+                        console.error(message)
+                        return
+                    case LogLevel.Info:
+                        console.info(message)
+                        return
+                    case LogLevel.Verbose:
+                        console.debug(message)
+                        return
+                    case LogLevel.Warning:
+                        console.warn(message)
+                        return
+                    default:
+                        return
+                }
+            },
+        },
+    },
 }
 
 export const loginRequest = {
