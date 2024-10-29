7d458f25d8d22a3acada2a8349885c56abcd2db6
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-29 10:15:48 +0100

    added outline and skeleton buttons for update and save with redirect

--- src/pages/EditUserProfilePage.tsx
+++ src/pages/EditUserProfilePage.tsx
@@ -1,20 +1,74 @@
 import React from 'react'
+import { Paths } from '../../paths'
 
 const EditUserProfilePage: React.FC = () => {
+    const handleSaveDetails = (e: React.FormEvent) => {
+        e.preventDefault()
+        const { name, email, phonenumber } = e.target as HTMLFormElement
+        location.replace(Paths.USER_PROFILE)
+    }
+
+    const handleUpdatePassword = (e: React.FormEvent) => {
+        e.preventDefault()
+        const { password, confirmPassword } = e.target as HTMLFormElement
+        location.replace(Paths.USER_PROFILE)
+    }
+
     return (
-        <div className="p-2 flex flex-col gap-3 w-full">
-            <label className="">
-                <p className="pl-2">Name</p>
-                <input required className="input-contained" name="title" />
-            </label>
-            <label className="">
-                <p className="pl-2">Email</p>
-                <input required className="input-contained" name="title" />
-            </label>
-            <label className="">
-                <p className="pl-2">Phonenumber</p>
-                <input required className="input-contained" name="title" />
-            </label>
+        <div className="flex flex-col gap-9 mt-10">
+            <form
+                className="flex flex-col gap-4 w-full"
+                onSubmit={handleSaveDetails}
+            >
+                <input
+                    required
+                    className="input-contained"
+                    name="name"
+                    placeholder="Name"
+                />
+                <input
+                    required
+                    className="input-contained"
+                    name="email"
+                    placeholder="Email"
+                />
+                <input
+                    required
+                    type="number"
+                    className="input-contained"
+                    name="phonenumber"
+                    placeholder="Phonenumber"
+                />
+                <button className="btn-primary" type="submit">
+                    Save details
+                </button>
+            </form>
+
+            <div className="flex flex-col gap-2">
+                <h3 className="text-lg">Update password</h3>
+                <form
+                    className="flex flex-col gap-4 w-full"
+                    onSubmit={handleUpdatePassword}
+                >
+                    <input
+                        required
+                        className="input-contained"
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                    />
+                    <input
+                        required
+                        className="input-contained"
+                        name="confirmPassword"
+                        placeholder="Repeat password"
+                        type="password"
+                    />
+                    <button className="btn-primary" type="submit">
+                        Update password
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
