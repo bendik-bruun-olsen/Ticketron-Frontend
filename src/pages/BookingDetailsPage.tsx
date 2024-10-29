0b467aa3daefd546ed7389a03fb48d7bf547332e
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-10-29 09:54:57 +0100

    remove duplicate navbar

--- src/pages/BookingDetailsPage.tsx
+++ src/pages/BookingDetailsPage.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React from 'react'
 
 const BookingDetailsPage: React.FC = () => {
-	return (
-		<div>
-			<Navbar />
-			<h2>Welcome to the Booking Details Page</h2>
-		</div>
-	);
-};
+    return (
+        <div>
+            <h2>Welcome to the Booking Details Page</h2>
+        </div>
+    )
+}
 
-export default BookingDetailsPage;
+export default BookingDetailsPage
