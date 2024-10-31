59646ef99ef22f0c50c151b218dd08cc58e94a15
Bendik Bruun-Olsen <bendik.bruun-olsen@bouvet.no>
2024-10-31 09:03:40 +0100

    Finished work with accessing .env variables in prod

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,11 @@ import router from '../routes'
 import Navbar from './components/Navbar'
 
 const App: React.FC = () => {
-    const env = import.meta.env.MODE
-    const env_loc = import.meta.env.VITE_ENV_NAME
-    const base_url = import.meta.env.VITE_BASE_URL
-    const clientId = import.meta.env.VITE_AZURE_AD_CLIENT_ID
-    const test = import.meta.env.VITE_TEST
+    const env = import.meta.env.MODE as string
+    const clientId = import.meta.env.VITE_AZURE_AD_CLIENT_ID as string
 
-    setTimeout(() => {
-        console.log('clientId with delay: ', clientId)
-    }, 1000)
-
-    console.log('clientId: ', clientId)
-    console.log('env_loc: ', env_loc)
+    console.log('clientId from App.tsx: ', clientId)
     console.log('env-mode: ', env)
-    console.log('base_url: ', base_url)
-    console.log('test: ', test)
 
     return <RouterProvider router={router} />
 }
