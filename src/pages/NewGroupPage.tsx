a497bc006feac4922972eed00e0d35d40f3d55a8
Jonas Alexander Støbakk <jonas.stobakk@bouvet.no>
2024-11-04 13:14:31 +0100

    Set up skeleton for NewGroupPage

--- src/pages/NewGroupPage.tsx
+++ src/pages/NewGroupPage.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from 'react'
+import { UserIcon, PlusIcon } from '@heroicons/react/24/outline'
 
 const NewGroupPage: React.FC = () => {
-	return (
-		<div>
-			<h1>Welcome to NewGroupPage</h1>
-		</div>
-	);
-};
+    const [groupName, setGroupName] = useState('')
 
-export default NewGroupPage;
+    const editGroupName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setGroupName(e.target.value)
+        console.log(groupName)
+    }
+
+    return (
+        <>
+            <div className="mt-4 ml-2 mr-2">
+                <h1 className="h1 ml-2 mb-2.5">Create a New Group</h1>
+                <input
+                    type="text"
+                    className="input-contained"
+                    placeholder="Group Name"
+                    value={groupName}
+                    onChange={editGroupName}
+                />
+            </div>
+            <div className="mt-7 ml-2 mr-2">
+                <h1 className="h1 ml-2 mb-2.5">Add members</h1>
+                <div className="relative">
+                    <UserIcon className="h-6 w-6 p-1 absolute box-border top-1/2 left-0.5 transform -translate-y-1/2 " />
+                    <input
+                        type="text"
+                        className="input-contained h-50 box-border pl-6"
+                        placeholder="Member Name"
+                    />
+                </div>
+            </div>
+            <PlusIcon className="fab h-10 w-10 mt-5 ml-3" color="white" />
+            <div className="flex justify-center">
+                <button className="btn-primary mt-24 w-11/12 justify-center">
+                    Save
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default NewGroupPage
