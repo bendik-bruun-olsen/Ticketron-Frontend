2a1414cab0574fd9455960b1ed25699bab8a32f2
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-11-05 11:44:25 +0100

    setup get bookings

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,48 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BookingCard } from '../components/Booking/BookingCard'
 import BookingsList from '../components/Booking/BookingsList'
 import { PlusCircleIcon, PlusIcon } from '@heroicons/react/24/solid'
 import { Paths } from '../../paths'
 import { useNavigate } from 'react-router-dom'
 import BackendTest from '../testing/BackendTest'
-
-const bookings = [
-    {
-        imageUrl: 'https://placehold.co/173x173',
-        title: 'Trip to Paris',
-        fromDate: 'June 10, 2023',
-        toDate: 'June 12, 2023',
-        participants: 1,
-        id: '1',
-    },
-    {
-        imageUrl: 'https://placehold.co/173x173',
-        title: 'Trip to Paris',
-        fromDate: 'June 10, 2023',
-        toDate: 'June 12, 2023',
-        participants: 1,
-        id: '2',
-    },
-    {
-        imageUrl: 'https://placehold.co/173x173',
-        title: 'Trip to Paris',
-        fromDate: 'June 10, 2023',
-        toDate: 'June 12, 2023',
-        participants: 1,
-        id: '3',
-    },
-]
+import { fetchData } from '../utils'
+import { useMsal } from '@azure/msal-react'
 
 const HomePage: React.FC = () => {
+    const { instance, accounts } = useMsal()
     const navigate = useNavigate()
     const handleClick = () => {
         navigate(Paths.ADD_BOOKING)
     }
 
+    const [bookings, setBookings] = useState([])
+
+    useEffect(() => {
+        const fetchBookings = async () => {
+            const data = await fetchData('/Booking/user/1')
+            setBookings(data)
+        }
+        fetchBookings()
+    }, [])
+
     return (
         <>
             <div className=" p-4 flex flex-col gap-10">
-                <h1 className="text-2xl font-bold">Welcome back, User!</h1>
+                <h1 className="text-2xl font-bold">
+                    Welcome back {accounts[0]?.name}!
+                </h1>
                 <BookingsList bookings={bookings} />
                 <button className="fab bottom-6 right-6" onClick={handleClick}>
                     <PlusIcon className="text-white size-6" />
