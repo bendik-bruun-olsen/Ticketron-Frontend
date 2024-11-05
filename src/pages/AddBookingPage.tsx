dbd73b5f560a5d3ea3a279f545426ce29fa956ac
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-11-05 11:44:35 +0100

    add post bookings

--- src/pages/AddBookingPage.tsx
+++ src/pages/AddBookingPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { postData } from '../utils'
 
 const AddNewBookingPage: React.FC = () => {
     const handleSubmit = (e: React.FormEvent): void => {
         e.preventDefault()
+        const formData = new FormData(e.target as HTMLFormElement)
+        const formProps = Object.fromEntries(formData)
         const { title, dateFrom, dateTo, participants } =
-            e.target as HTMLFormElement
+            formProps as HTMLFormElement
+
+        try {
+            postData('/Booking/create', {
+                title: title,
+                startDate: dateFrom,
+                endDate: dateTo,
+                userId: 1,
+            })
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     return (
@@ -31,11 +45,7 @@ const AddNewBookingPage: React.FC = () => {
 
             <label className="p-2">
                 Participants
-                <input
-                    required
-                    className="input-contained"
-                    name="participants"
-                />
+                <input className="input-contained" name="participants" />
             </label>
             <button className="btn-primary ml-2" type="submit">
                 Save
