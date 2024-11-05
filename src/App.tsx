18a47d9736ce8e8ca0956a58ef92700901b32d75
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-05 09:56:35 +0100

    Deleted comment and Navbar import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-// App.tsx
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from '../routes'
-import Navbar from './components/Navbar'
 
 const App: React.FC = () => {
     return <RouterProvider router={router} />
