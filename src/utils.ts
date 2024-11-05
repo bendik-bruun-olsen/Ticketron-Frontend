b5217b55c78a11a0437b31a7fed314820eb2ae93
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-11-05 14:19:51 +0100

    added post and fetch generic function

--- src/utils.ts
+++ src/utils.ts
@@ -44,7 +44,7 @@ export const fetchData = async (url: string) => {
 //console.log(id) -> This will get the id of the created booking
 export const postData = async (
     url: string,
-    body: Record<string, string | number>
+    body: Record<string, string | number | undefined>
 ) => {
     const account = msalInstance.getActiveAccount()
     if (!account) {
