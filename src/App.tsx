38e25c4b3fb7ea58e7111684fb7dbce1f17dace0
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-11-05 11:43:47 +0100

    setup fetch and post with msal

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from '../routes'
-import Navbar from './components/Navbar'
 
 const App: React.FC = () => {
     return <RouterProvider router={router} />
