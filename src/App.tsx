385123e2efa2bbf8e41b03d98517ee1e0cc982b4
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-07 09:12:29 +0100

    Applied the Error Boundary to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from '../routes'
+import ErrorBoundary from './components/Error/ErrorBoundary'
 
 const App: React.FC = () => {
-    return <RouterProvider router={router} />
+    return (
+        <ErrorBoundary>
+            <RouterProvider router={router} />
+        </ErrorBoundary>
+    )
 }
 
 export default App
