73709361b38552e14c639f995913646cdf989b37
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-08 14:27:16 +0100

    created a route and path for bookingsPage

--- src/pages/AddTicketPage.tsx
+++ src/pages/AddTicketPage.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import TicketForm from '../components/Ticket/TicketForm'
+import { postData } from '../utils'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Ticket } from '../components/types'
 
 const AddTicketPage: React.FC = () => {
-    const handleAddTicket = (ticketdata: any) => {
-        console.log('Adding Ticket', ticketdata)
-    }
+    const { bookingId } = useParams<{ bookingId: string }>()
+    const navigate = useNavigate()
 
+    // const newTicket.Id = 1
+
+    const handleAddTicket = async (ticket: Ticket) => {
+        const body = {
+            title: ticket.title,
+            participantId: 2,
+            startDate: new Date(ticket.startDate).toISOString(),
+            endDate: new Date(ticket.endDate).toISOString(),
+            userId: 1,
+            bookingId: bookingId,
+        }
+        try {
+            const newTicket = await postData(`/Ticket/create`, body)
+            navigate(`./tickets/${newTicket.id}`)
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return (
         <div>
             <h1>Add Ticket</h1>
--- src/pages/BookingPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const BookingPage: React.FC = () => {
-    return <div className="p-4"></div>
-}
-
-export default BookingPage
