0e4951a30dc9691ec4bc6691c4ee4433fb57600f
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-11 09:53:32 +0100

    Fixed the Search Icon and text spacing

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -4,12 +4,12 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 const SearchFilter = () => {
     return (
         <div className=" p-4 flex flex-col gap-10">
-            <div className="mb-4">
-                <MagnifyingGlassIcon className="p-1 absolute box-border top-1/2 size-7 left-0.5 transform -translate-y-1/2 " />
+            <div className=" relative mb-4">
+                <MagnifyingGlassIcon className="absolute top-1/2 left-3 transform -translate-y-1/2 h-5 w-5 text-gray-600" />
                 <input
                     type="text"
                     placeholder="Search..."
-                    className="w-full px-4 py-2 rounded-full bg-gray-200 text-gray-600"
+                    className="w-full pl-10 px-4 py-2 rounded-full bg-gray-200 text-gray-600"
                 />
             </div>
         </div>
