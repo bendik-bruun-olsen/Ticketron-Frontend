4801011d766cc1139dfbe8dbe203126fb6b98ab7
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-14 13:14:19 +0100

    Changed the UserID

--- src/pages/BookingsPage.tsx
+++ src/pages/BookingsPage.tsx
@@ -18,9 +18,7 @@ const BookingPage: React.FC = () => {
 
     useEffect(() => {
         const fetchBookings = async () => {
-            const data = await fetchData(
-                '/Booking/user/ae943f62-e749-420b-b8b4-74d08f506f44'
-            )
+            const data = await fetchData('/Booking/user/1')
             setBookings(data)
         }
         fetchBookings()
