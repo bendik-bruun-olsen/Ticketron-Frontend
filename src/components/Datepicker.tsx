93ccdc1516e77c5b78c5f41d7a4c1f3072482935
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-11-14 12:59:45 +0100

    added new datepicker

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import 'flowbite/dist/flowbite.css'
 import React from 'react'
 import { CalendarIcon } from '@heroicons/react/24/solid'
-import { Datepicker } from 'flowbite-react'
+import { CustomFlowbiteTheme, Datepicker, Flowbite } from 'flowbite-react'
 
 interface DaterangePickerProps {
     dateRange: {
@@ -17,32 +17,52 @@ interface DaterangePickerProps {
     >
 }
 
+const customTheme: CustomFlowbiteTheme = {
+    textInput: {
+        field: {
+            input: {
+                base: 'input-contained-icon',
+            },
+        },
+    },
+}
+
 const DaterangePicker = ({ dateRange, setDateRange }: DaterangePickerProps) => {
     return (
-        <>
-            <div className="mb-4">
+        <Flowbite theme={{ theme: customTheme }}>
+            <div>
+                <label
+                    htmlFor="datepicker-range-start"
+                    className="block text-sm pl-2 font-medium text-gray-900 dark:text-white"
+                >
+                    Start Date
+                </label>
                 <Datepicker
                     name="startDate"
                     id="startDate"
-                    language="en"
-                    className="input-contained"
                     onChange={(date) =>
                         setDateRange({ ...dateRange, startDate: date })
                     }
                     value={dateRange.startDate ?? new Date()}
-                />
+                />{' '}
             </div>
-            <div className="mb-4">
+            <div>
+                <label
+                    htmlFor="datepicker-range-end"
+                    className="block text-sm pl-2 font-medium text-gray-900 dark:text-white"
+                >
+                    End Date{' '}
+                </label>
+
                 <Datepicker
                     name="endDate"
                     onChange={(date) =>
                         setDateRange({ ...dateRange, endDate: date })
                     }
-                    id="input-contained"
                     value={dateRange.endDate ?? new Date()}
                 />
             </div>
-        </>
+        </Flowbite>
     )
 }
 
