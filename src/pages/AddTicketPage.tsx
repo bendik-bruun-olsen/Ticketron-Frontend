a640c38bb43ff59fa1b0abb3ea9ee7c3e757135d
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-18 10:30:32 +0100

    Added Edit ticket function

--- src/pages/AddTicketPage.tsx
+++ src/pages/AddTicketPage.tsx
@@ -9,25 +9,10 @@ const AddTicketPage: React.FC = () => {
     const { bookingId } = useParams<{ bookingId: string }>()
     const navigate = useNavigate()
     const [tickets, setTickets] = useState<Ticket[]>([])
-
+    const { instance, accounts } = useMsal()
     // const newTicket.Id = 1
 
-    useEffect(() => {
-        const getTickets = async () => {
-            try {
-                const data: Ticket[] = await fetchData(
-                    `/tickets?bookingId=${bookingId}`
-                )
-                setTickets(data)
-            } catch (error) {
-                console.error('Error fetching tickets:', error)
-            }
-        }
-        getTickets()
-    }, [bookingId])
-
     const handleAddTicket = async (ticket: Ticket) => {
-        const { instance, accounts } = useMsal()
         const body = {
             title: ticket.title,
             participantId: 2,
--- src/pages/EditTicketPage.tsx
+++ src/pages/EditTicketPage.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TicketForm from '../components/Ticket/TicketForm'
+import { Ticket } from '../components/types'
+import { fetchData } from '../utils'
+import { useParams } from 'react-router-dom'
 
 const EditTicketPage: React.FC<{ initialData: any }> = ({ initialData }) => {
+    const { bookingId } = useParams<{ bookingId: string }>()
+    const [tickets, setTickets] = useState<Ticket[]>([])
+
+    useEffect(() => {
+        const getTickets = async () => {
+            try {
+                const data: Ticket[] = await fetchData(`/ticket`)
+                setTickets(data)
+            } catch (error) {
+                console.error('Error fetching tickets:', error)
+            }
+        }
+        getTickets()
+    }, [bookingId])
+
     const handleEditTicket = (ticketdata: any) => {
         console.log('Adding Ticket', ticketdata)
     }
