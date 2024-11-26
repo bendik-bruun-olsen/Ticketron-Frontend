8aa30587d4bd44ac2490e542b999c766332e253a
Kokila Chandramohan <kokila.kodehode@gmail.com>
2024-11-26 10:13:29 +0100

    Added Ticket Sorting according to Category choosen in the Dropdown

--- src/pages/BookingDetailsPage.tsx
+++ src/pages/BookingDetailsPage.tsx
@@ -7,11 +7,17 @@ import SearchFilter from '../components/SearchFilter'
 import { Dropdown } from 'flowbite-react'
 import { fetchData } from '../utils'
 import Snackbar from '../components/Snackbar'
+import { Categories } from '../components/types'
+import { categoriesArray } from '../utils'
 
 const BookingDetailsPage: React.FC = () => {
     const { bookingId } = useParams<{ bookingId: string }>()
     const navigate = useNavigate()
     const [tickets, setTickets] = useState<any[]>([])
+    const [filteredTickets, setFilteredTickets] = useState<any[]>([])
+    const [selectedCategory, setSelectedCategory] = useState<Categories | null>(
+        null
+    )
     const [snackbar, setSnackbar] = useState<{
         message: string
         type: 'success' | 'error' | 'info'
@@ -30,6 +36,7 @@ const BookingDetailsPage: React.FC = () => {
                 if (!response.ok) throw new Error('Failed to fetch tickets')
                 const data = await response.json()
                 setTickets(data)
+                setFilteredTickets(data)
                 setSnackbar({
                     message: 'Tickets fetched successfully!',
                     type: 'success',
@@ -47,28 +54,17 @@ const BookingDetailsPage: React.FC = () => {
         fetchTickets()
     }, [bookingId])
 
-    // const tickets = [
-    //     {
-    //         imageUrl: 'https://via.placeholder.com/64',
-    //         title: 'Ticket Name',
-    //         type: 'Flybillett',
-    //         username: 'Bruker Navn',
-    //         price: '1000kr',
-    //         startDate: ' 10.10.2024',
-    //         endDate: ' 10.10.2024',
-    //         id: '1',
-    //     },
-    //     {
-    //         imageUrl: 'https://via.placeholder.com/64',
-    //         title: 'Ticket Name',
-    //         type: 'Flybillett',
-    //         username: 'Bruker Navn',
-    //         price: '1000kr',
-    //         startDate: ' 10.10.2024',
-    //         endDate: ' 10.10.2024',
-    //         id: '2',
-    //     },
-    // ]
+    const filterTicketsByCategory = (category: Categories | '') => {
+        setSelectedCategory(category)
+        if (category) {
+            const filtered = tickets.filter(
+                (ticket) => ticket.category === category
+            )
+            setFilteredTickets(filtered)
+        } else {
+            setFilteredTickets(tickets)
+        }
+    }
 
     const goToAddTicketPage = () => {
         navigate(`/booking/${bookingId}/add-ticket`)
@@ -85,19 +81,23 @@ const BookingDetailsPage: React.FC = () => {
         <div className="p-4 bg-gray-100 min-h-screen relative">
             <SearchFilter></SearchFilter>
             <div className="flex flex-row items-baseline">
-                <h2 className="text-xl font-bold mb-4">Plane tickets</h2>
-                <Dropdown inline label="Options">
-                    <Dropdown.Item onClick={goToAddTicketPage}>
-                        Add Ticket
+                <h2 className="text-2xl font-bold">Tickets</h2>
+                <Dropdown inline label="Category">
+                    <Dropdown.Item onClick={() => filterTicketsByCategory('')}>
+                        All
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={goToEditTicketPage}>
-                        Edit Ticket
-                    </Dropdown.Item>
+                    {categoriesArray.map((category) => (
+                        <Dropdown.Item
+                            onClick={() => filterTicketsByCategory(category)}
+                        >
+                            {category}
+                        </Dropdown.Item>
+                    ))}
                 </Dropdown>
             </div>
 
             <div className="space-y-4">
-                {tickets.map((ticket, index) => (
+                {filteredTickets.map((ticket, index) => (
                     <TicketCard key={index} {...ticket} />
                 ))}
                 <button
