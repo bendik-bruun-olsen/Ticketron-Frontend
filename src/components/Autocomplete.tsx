14441508250be7f861f9850a0d6d4f142b5d09a9
Anne Skjæveland <anne.skjeveland@bouvet.no>
2024-11-28 12:26:27 +0100

    fixed height of autocomplete

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -102,7 +102,7 @@ export const Autocomplete = ({
                 aria-label="Search"
             />
             {filteredSuggestions && showSuggestions && (
-                <div className="absolute z-10 w-full mt-1 bg-white border rounded-md shadow-lg">
+                <div className="absolute z-10 w-full mt-1 bg-white border rounded-md shadow-lg max-h-72 overflow-scroll">
                     {filteredSuggestions.map((suggestion, index) => (
                         <div
                             className="px-4 py-2 cursor-pointer hover:bg-gray-200 flex gap-2 justify-between"
